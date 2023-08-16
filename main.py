0056478958a3116ce6d1de98c3bfe7ae107296eb
Leonardo Ccorahua Madera <137753453+leodev17@users.noreply.github.com>
2023-08-16 10:17:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,14 @@
-import streamlit as st
-import numpy as np
-import networkx as nx
-def grafo_desde_matriz(matriz):
-    grafo = nx.Graph()
-    # Obtener el número de nodos en la matriz
-    num_nodos = A.shape[0]
+from functions import *
 
-    # Agregar nodos al grafo
-    grafo.add_nodes_from(range(1, num_nodos + 1))  # Supongamos que los nodos están numerados desde 1
+st.header("Prueba de proyecto fhasdjhkfa")
+st.subheader("Hola estimado usuario. Para continuar el analisis se requiere una matriz simetrica nxn, n>2")
+num_aceptados = np.arange(30) + 3
+num_aceptados = num_aceptados.astype(str)
+tam = st.text_input("¿De que tamaño desea ingresar su matriz de adyacencia?")
+if len(tam) > 0:
+    if tam not in num_aceptados:
+        st.error("Numero invalido")
+    else:
+        tam = int(tam)
+        pedir_matriz(tam)
 
-    # Recorrer la matriz de adyacencia y agregar las conexiones al grafo
-    for i in range(num_nodos):
-        for j in range(i + 1, num_nodos):
-            if A[i, j] == 1:
-                grafo.add_edge(i + 1, j + 1)
-    return grafo
-
-A = np.matrix([[0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0],
-               [0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0],
-               [0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-               [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-               [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-               [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
-               [0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0],
-               [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-               [1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
-               [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-               [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1]])
-grafo=grafo_desde_matriz(A)
-dot = nx.nx_pydot.to_pydot(grafo)
-st.graphviz_chart(dot.to_string())
-componentes_conexas = list(nx.connected_components(grafo))
-
-# Imprimir las componentes conexas
-for i, componente in enumerate(componentes_conexas):
-    st.text("Componente " + str(i + 1) + ": " + str(componente))
